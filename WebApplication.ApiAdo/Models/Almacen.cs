d5da1d0763375bbd60e20f6914880d9ed0547cfb
Emiliano Di Noto <emi04014@gmail.com>
2024-10-19 00:48:25 -0300

    Primer subida de proyecto 19/10 a las 00:44 Agregué las conexiones a BD de cada uno. Queda modificar con sus bases de datos.

--- WebApplication.ApiAdo/Models/Almacen.cs
+++ WebApplication.ApiAdo/Models/Almacen.cs
@@ -12,8 +12,18 @@ namespace WebApplication.ApiAdo.Models
 
         #region Atributo
 
+
+        // Conexión a Base de Datos EMI
         string conectionString = @"Data Source=EMI-PC\EMI_PC_SERVER;Initial Catalog=GridoTechAdvance ; Integrated Security= True ";
 
+        // Conexión a Base de Datos EMMA
+        //string conectionString = @"Data Source= ;Initial Catalog=GridoTechAdvance ; Integrated Security= True ";
+
+        // Conexión a Base de Datos DIANA
+        //string conectionString = @"Data Source=;Initial Catalog=GridoTechAdvance ; Integrated Security= True ";
+
+        // Conexión a Base de Datos SOFIA
+        //string conectionString = @"Data Source=;Initial Catalog=GridoTechAdvance ; Integrated Security= True ";
 
         #endregion
 
