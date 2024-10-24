27c3bc330e4d889eb6f6d5d85877682db3014bec
dianita14 <zavaletadiana643@gmail.com>
2024-10-24 18:14:47 -0300

    Agregue controller de inventory

--- WebApplication.ApiAdo/Models/Almacen.cs
+++ WebApplication.ApiAdo/Models/Almacen.cs
@@ -14,13 +14,13 @@ namespace WebApplication.ApiAdo.Models
 
 
         // Conexión a Base de Datos EMI
-        string conectionString = @"Data Source=EMI-PC\EMI_PC_SERVER;Initial Catalog=GridoTechAdvance ; Integrated Security= True ";
+        //string conectionString = @"Data Source=EMI-PC\EMI_PC_SERVER;Initial Catalog=GridoTechAdvance ; Integrated Security= True ";
 
         // Conexión a Base de Datos EMMA
         //string conectionString = @"Data Source= ;Initial Catalog=GridoTechAdvance ; Integrated Security= True ";
 
         // Conexión a Base de Datos DIANA
-        //string conectionString = @"Data Source=;Initial Catalog=GridoTechAdvance ; Integrated Security= True ";
+        string conectionString = @"Data Source=DIANA\SQLEXPRESS01;Initial Catalog=GridoTech ; Integrated Security= True ";
 
         // Conexión a Base de Datos SOFIA
         //string conectionString = @"Data Source=;Initial Catalog=GridoTechAdvance ; Integrated Security= True ";
